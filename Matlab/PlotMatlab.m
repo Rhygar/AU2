3e8370d12366e096cab8dbd73271a50eb73a9157
Rhygar <john_tengvall@hotmail.com>
2015-12-08 11:07:56 +0100

    Har lagt till plot 2, Kolla igenom kommentarer
    Förstår inte riktigt vad vinkelhastigheten skall vara på plot 1.
    Värdena ska ändras på plot 2 och man ska beskriva vad som händer.

--- Matlab/PlotMatlab.m
+++ Matlab/PlotMatlab.m
@@ -1,3 +1,11 @@
+%Plotta funktionen F(t)=bcos(?t)för b>0 i Matlab/Octave.
+%Frågan saknar värde för vinkelhastighet
 x = 0:0.01:2*pi;
 b = 4;
 plot(x,b.*cos(x.*(2.*pi)))
+%Plotta lösningen i en graf i Matlab/Octave för A = 3 , ? = ? / 2 och ?0 = 1
+x = 0:0.1:2*pi;
+A = 3;
+delta = pi/2;
+vinkel = 1;
+plot(A*sin((vinkel.*x) + delta))
